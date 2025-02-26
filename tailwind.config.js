5882e1123fb9847d3d6ae8a046833df62b085e23
adarsh0707-kumar <adarshku123456789@gmail.com>
2025-02-26 20:29:34 +0530

    Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,10 @@
-
-export default  {
+export default {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
