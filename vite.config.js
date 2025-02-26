a17ed1ef7d6aff239c447864e3aafc9cbf850f02
adarsh0707-kumar <adarshku123456789@gmail.com>
2025-02-26 21:33:49 +0530

    CSS update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    
-  ],
+  plugins: [react()],
+  server: {
+    port: 3000, // You can change this port number if needed
+    open: true, // Automatically opens the browser when you start the server
+  },
+  build: {
+    outDir: 'dist', // The folder where the build files will be output
+  },
 });
